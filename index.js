7e445518d9857506fd8f6d55befec6f8e15f2ff3
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-05-08 18:20:33 +0400

    lines

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById('canvas')
 canvas.width = 800
 canvas.height = 500
-var c = canvas.getContext('2d')
-c.fillRect(0, 0, 500, 300)
+const c = canvas.getContext('2d')
+
+c.fillRect(0, 0, 200, 150)
+c.fillRect(400, 0, 200, 150)
+c.fillRect(200, 150, 200, 150)
+
 console.log(canvas)
+const x = 0
+const y = 250
+
+// line
+c.beginPath()
+c.moveTo(x, y)
+c.lineTo(200, 400)
+c.lineTo(350, 400)
+c.lineTo(550, 300)
+c.lineTo(700, 450)
+c.lineTo(800, 350)
+c.stroke()
