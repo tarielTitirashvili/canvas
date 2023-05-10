9694b2447f587d4abf1d4f29f7d43bdaf8862e13
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-05-10 18:40:57 +0400

    needs map

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas')
-canvas.width = 800
-canvas.height = 500
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 const c = canvas.getContext('2d')
 c.fillStyle = 'rgba(168,120,150,100)'
 c.fillRect(0, 0, 200, 150)
@@ -36,3 +36,20 @@ c.arc(
   )
 c.strokeStyle='red'
 c.stroke()
+
+const radius = Math.round(Math.random()*200)
+const generateWidth = ()=>Math.random()*window.innerWidth+Math.round(radius/2)
+
+for (let i=0; i<100; i++){
+  c.beginPath()
+  c.arc(
+    generateWidth(), //
+    Math.random()*window.innerHeight-radius, // position
+    radius, //radius
+    0, //radiants start?
+    600, //radiants end?
+    false//clockWithe
+    )
+  c.strokeStyle='red'
+  c.stroke()
+}
